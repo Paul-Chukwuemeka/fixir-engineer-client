1a1b69576d12ce537342252f246be432e026d83a
paul-chukwuemeka <paulchukwuemeka123@gmail.com>
2025-09-26 18:40:42 +0100

    Refactor POST function in route.ts for improved readability and consistency

--- src/app/api/getData/route.ts
+++ src/app/api/getData/route.ts
@@ -1,11 +1,12 @@
 import { NextResponse } from "next/server";
 
-
-export default async function POST(req:Request){
-    const body = await req.json() 
+export async function POST(req: Request) {
+    const body = await req.json();
     try {
-        console.log(body)
+        console.log(body);
+
+        return NextResponse.json({ message: "Request successful", data: body });
     } catch (error) {
-        return NextResponse.json({message:"Request failed",error})
+        return NextResponse.json({ message: "Request failed", error });
     }
 }
